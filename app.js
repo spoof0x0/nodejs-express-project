b836b832f2bcd50a4885e0fff6e7b971c780dfa7
Ahmed Abo Bakr <ahmedabobakr918@gmail.com>
2026-05-09 05:36:51 +0300

    fix port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const User = require('./models/user');
 app.set('view engine', 'ejs');
